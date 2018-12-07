d568a29ce87b9215adde701dd09aa7ef5f51b520
babluchandran <bc00434278@techmahindra.com>
2018-12-07 13:21:08 +0530

    Update TestController.java

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-	@Value("${emp.name}")
+	@Value("${emp_name}")
 	private String firstName;
 	
 		
