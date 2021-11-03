ec75452ff0fb0590fec64b6a8aead24c12762a2f
babluchandran <mailtobabluchandran@gmail.com>
2021-11-03 16:06:47 +0530

    Update TestController.java

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -16,7 +16,7 @@ public class TestController {
 		
     @RequestMapping(value ="/api/**",method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public Widget index() {
-        return new Widget(${emp_name}, 10, 7);
+        return new Widget("Bablu", 10, 7);
     }
     
     
